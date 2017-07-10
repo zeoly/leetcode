301e946c5d9d7b7baee31e5b87deacded4c9e258
zeoly <zeoly100@163.com>
2017-07-10 10:01:39 +0800

    Palindrome Number

---
+++ te-leetcode/src/main/java/com/thinkequip/leetcode/PalindromeNumber.java
@@ -0,0 +1,22 @@
+package com.thinkequip.leetcode;
+
+public class PalindromeNumber {
+
+	public boolean isPalindrome(int x) {
+		if (x < 0) {
+			return false;
+		}
+		int temp = x;
+		int result = 0;
+		while (temp > 0) {
+			if (result > Integer.MAX_VALUE / 10) {
+				result = 0;
+				break;
+			}
+			result = result * 10 + temp % 10;
+			temp = temp / 10;
+		}
+		return x == result ? true : false;
+	}
+
+}
