01f2d652c47e0096e7ce8e920f70be68d1836939
zeoly <zeoly100@163.com>
2017-07-10 10:06:22 +0800

    Valid Parentheses

---
+++ te-leetcode/src/main/java/com/thinkequip/leetcode/ValidParentheses.java
@@ -0,0 +1,44 @@
+package com.thinkequip.leetcode;
+
+public class ValidParentheses {
+
+	public boolean isValid(String s) {
+		char[] array = s.toCharArray();
+		char[] stack = new char[array.length];
+		int point = 0;
+		for (int i = 0; i < array.length; i++) {
+			if ('(' == array[i] || '[' == array[i] || '{' == array[i]) {
+				stack[point] = array[i];
+				point++;
+			} else if (point == 0) {
+				return false;
+			} else if (')' == array[i]) {
+				if (stack[point - 1] == '(') {
+					point--;
+				} else {
+					return false;
+				}
+			} else if (']' == array[i]) {
+				if (stack[point - 1] == '[') {
+					point--;
+				} else {
+					return false;
+				}
+			} else if ('}' == array[i]) {
+				if (stack[point - 1] == '{') {
+					point--;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+		if (point == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
