ce18aba1b7928cd760becd042d8509afc6354d2d
zeoly <zeoly100@163.com>
2017-07-10 17:47:29 +0800

    Remove Duplicates From Sorted Array

---
+++ te-leetcode/src/main/java/com/thinkequip/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,30 @@
+package com.thinkequip.leetcode;
+
+public class RemoveDuplicatesFromSortedArray {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 1, 2, 2, 3, 4, 5, 5, 6 };
+		int result = removeDuplicates(nums);
+		System.out.println(result);
+		for (int i = 0; i < nums.length; i++) {
+			System.out.print(nums[i] + ",");
+		}
+	}
+
+	public static int removeDuplicates(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+		int current = nums[0];
+		int index = 0;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] > current) {
+				current = nums[i];
+				index++;
+				nums[index] = current;
+			}
+		}
+		return index + 1;
+	}
+
+}
